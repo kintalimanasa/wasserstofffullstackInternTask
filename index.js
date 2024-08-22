9d9d114f24448f507e8c238b01b08c08cba1fc8b
kintali Manasa <84638159+kintalimanasa@users.noreply.github.com>
2024-08-22 12:08:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WeatherForecast from './WeatherForecast';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WeatherForecast />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
